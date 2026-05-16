e27688661e9b902f8dbaf3cc264e4d5cd349a614
hemantsharma-codes <golubhardwaj8126@gmail.com>
2026-05-16 16:54:14 +0530

    Revert "Merge pull request #4 from hemantsharma-codes/feature/UC4-enrollment-service"
    This reverts commit 2edc184f00217ddc22d566730b360aa5d7ed1afd, reversing
    changes made to 5c0bac2f3a48594dbcd335bf0a47991bc25dda8e.

--- EduLearn.AuthService/DTOs/RefreshTokenRequestDto.cs
+++
@@ -1,8 +0,0 @@
-namespace EduLearn.AuthService.DTOs
-{
-    public class RefreshTokenRequestDto
-    {
-        public required string Token { get; set; }
-        public required string RefreshToken { get; set; }
-    }
-}
--- EduLearn.AuthService/Models/JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace EduLearn.AuthService.Models
-{
-    public class JwtSettings
-    {
-        public required string SecretKey { get; set; }
-        public required string Issuer { get; set; }
-        public required string Audience { get; set; }
-    }
-}
